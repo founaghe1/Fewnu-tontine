68b0a80f250992d60270f0cf9f08f1c84ad8310f
Princetoutcouleur <babathiam0000@gmail.com>
2023-09-27 16:38:56 +0000

    Integration page profil

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.js'
 import Accueil from './Components/ComponentUser/Accueil/Accueil';
 import Cotisation from './Components/ComponentUser/Cotisation/Cotisation';
+import Profil from './Components/ComponentUser/Profil/Profil';
 
 function App() {
   return (
     <div className="App">
       {/* <Accueil/> */}
-      <Cotisation/>
+      {/* <Cotisation/> */}
+      <Profil/>
     </div>
   );
 }
