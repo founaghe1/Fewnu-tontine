29d0e9c5be89ae2546c7c3f0302550b1128b76b1
Mohamed Diallo <mohameddiallo.msd11@gmail.com>
2023-10-02 10:11:04 +0000

    rectifiction footer

--- src/Components/ComponentUser/Layout/Layout.jsx
+++ src/Components/ComponentUser/Layout/Layout.jsx
@@ -6,7 +6,7 @@ const Layout = ({ children }) =>  {
   return (
     <div>
       <Header />
-      <main className='mt-5 pt-5'>{children}</main>
+      <main className='my-5 py-5'>{children}</main>
       <Footer />
     </div>
   );
