9f79bf2ce7ed0841d1c4eb115336f85f3c8c165e
Princetoutcouleur <babathiam0000@gmail.com>
2023-10-02 12:36:16 +0000

    page detail tontine

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js'
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Detailcotisation from './Components/ComponentUser/Detailscotisations/Detailcotisation/Detailcotisation';
-import Accueil from './Components/ComponentUser/Accueil/Accueil';
-import Cotisation from './Components/ComponentUser/Cotisation/Cotisation'
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Detailcotisation from "./Components/ComponentUser/Detailscotisations/Detailcotisation/Detailcotisation";
+import Accueil from "./Components/ComponentUser/Accueil/Accueil";
+import Cotisation from "./Components/ComponentUser/Cotisation/Cotisation";
+import Tontine from "./Components/ComponentUser/Tontine/Tontine";
 
 function App() {
   return (
     <div className="">
-      <Router>
-      <Routes>
-        <Route path="/" exact element={<Accueil />} />
-        <Route path="/cotisation"  element={<Cotisation />} />
-      </Routes>
-    </Router>
+      <Tontine/>
+      {/* <Router>
+        <Routes>
+          <Route path="/" exact element={<Accueil />} />
+          <Route path="/cotisation" element={<Cotisation />} />
+        </Routes>
+      </Router> */}
     </div>
   );
 }
