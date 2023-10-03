bbb8a479c2160ff76f703d7603786aab605ded63
saliou thioune <bayezalle995@gmail.com>
2023-10-03 18:20:33 +0000

    nouvea

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js'
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Detailcotisation from './Components/ComponentUser/Detailscotisations/Detailcotisation/Detailcotisation';
+// import Detailcotisation from './Components/ComponentUser/Detailscotisations/Detailcotisation/Detailcotisation';
 import Accueil from './Components/ComponentUser/Accueil/Accueil';
 import Cotisation from './Components/ComponentUser/Cotisation/Cotisation'
 import Identification from './Components/ComponentUser/Inscription/IdentificationTelPin/Identification';
 import InfoPersonnelle from './Components/ComponentUser/Inscription/InfoPersonnelle/InfoPersonnelle';
-import Connection from './Components/ComponentUser/Inscription/Connection/Connection';
+// import Connection from './Components/ComponentUser/Inscription/Connection/Connection';
 import Confime from './Components/ComponentUser/ConfimeNumero/Confime';
 import Creercode from './Components/ComponentUser/creercode/Creercode';
 import Tontine from './Components/ComponentUser/Tontine/Tontine'
@@ -17,6 +17,7 @@ function App() {
   return (
     <div className="">
       <Router>
+        <TypeTontine/>
       <Routes>
         <Route path="/" exact element={<Accueil />} />
         <Route path="/idParCall"  element={<Identification />} />
