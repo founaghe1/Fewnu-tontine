46ce431f437a35e1febb491db5d09577a7b81f68
Mohamed Diallo <mohameddiallo.msd11@gmail.com>
2023-10-15 23:10:48 +0000

    deployment render .env2

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const connectDB = async () => {
   MONGODB_URL=`mongodb+srv://founaghe:founagheatlas@cluster0.ly68n1b.mongodb.net/fewnu-tontin?retryWrites=true&w=majority`
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    await mongoose.connect(process.env.MONGODB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
