68d3862f6a9de1668a573ef4221e22c98b5d3bbb
Mohamed Diallo <mohameddiallo.msd11@gmail.com>
2023-10-23 15:44:44 +0000

    association du user et cotisation par l'id en comparant le phoneNumber1

--- backend/controllers/addCotisation.js
+++ backend/controllers/addCotisation.js
@@ -11,19 +11,19 @@ const addCotisation = async (req, res, next) => {
 
         // Recherchez l'utilisateur en fonction du numéro de téléphone
         const user = await User.findOne({ phoneNumber: phoneNumberCot });
-        
+
         if (!user) {
         return res.status(404).json({ message: 'Utilisateur introuvable' });
         }
 
-        // Vérifiez si le numéro de téléphone correspond
-        if (user.phoneNumber !== phoneNumberCot) {
-            console.log(user.phoneNumber);
-            console.log(phoneNumberCot);
-            return res.status(400).json({ message: 'Le numéro de téléphone ne correspond pas à l\'utilisateur',
-            user, phoneNumberCot
-        });
-        }
+        // // Vérifiez si le numéro de téléphone correspond
+        // if (user.phoneNumber !== phoneNumberCot) {
+        //     console.log(user.phoneNumber);
+        //     console.log(phoneNumberCot);
+        //     return res.status(400).json({ message: 'Le numéro de téléphone ne correspond pas à l\'utilisateur',
+        //     user, phoneNumberCot
+        // });
+        // }
 
         // Créez la cotisation en associant l'ID de l'utilisateur
         const CotisationMtn = new Cotisation({ cotisation, user: user._id });
