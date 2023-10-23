6e54805aa979df106f85f07c348968784c9ec620
Mohamed Diallo <mohameddiallo.msd11@gmail.com>
2023-10-23 13:50:42 +0000

    association du user et cotisation2

--- backend/controllers/addCotisation.js
+++ backend/controllers/addCotisation.js
@@ -5,18 +5,18 @@ const User = require('../models/User');
 
 // Register a new Cotisation
 const addCotisation = async (req, res, next) => {
-    const { cotisation, userId } = req.body;
+    const { cotisation, username } = req.body;
   
     try {
 
         // vérifier si l'utilisateur existe avant de créer la cotisation
-        const user = await User.findById(userId);
+        const user = await User.findOne(username);
         if (!user) {
         return res.status(404).json({ message: 'Utilisateur introuvable' });
         }
 
         // Créez la cotisation en associant l'ID de l'utilisateur
-        const CotisationMtn = new Cotisation({ cotisation, user: userId });
+        const CotisationMtn = new Cotisation({ cotisation, user: user_Id });
         await CotisationMtn.save();
         res.json({ message: 'Cotisation Registration success' });
   
--- backend/models/Cotisation.js
+++ backend/models/Cotisation.js
@@ -10,7 +10,7 @@ const cotisationSchema = new mongoose.Schema(
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User', // Référence au modèle User
-      }
+    }
   },
   { timestamps: true }
 
