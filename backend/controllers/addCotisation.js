063e67f120d3d6f6993e71001b0532cdae55bde4
Mohamed Diallo <mohameddiallo.msd11@gmail.com>
2023-10-23 15:49:56 +0000

    association du user et cotisation par l'id en comparant le phoneNumber2

--- backend/controllers/addCotisation.js
+++ backend/controllers/addCotisation.js
@@ -1,38 +1,26 @@
 const Cotisation = require('../models/Cotisation');
 const User = require('../models/User');
 
-
-
-// Register a new Cotisation
+// Enregistrez une nouvelle Cotisation en associant automatiquement l'ID de l'utilisateur
 const addCotisation = async (req, res, next) => {
-    const { cotisation, username, phoneNumberCot } = req.body;
-  
-    try {
+  const { cotisation, phoneNumberCot } = req.body; // Récupérez le numéro de téléphone à partir du corps de la requête
 
-        // Recherchez l'utilisateur en fonction du numéro de téléphone
-        const user = await User.findOne({ phoneNumber: phoneNumberCot });
+  try {
+    // Recherchez l'utilisateur en fonction du numéro de téléphone
+    const user = await User.findOne({ phoneNumber: phoneNumberCot });
 
-        if (!user) {
-        return res.status(404).json({ message: 'Utilisateur introuvable' });
-        }
-
-        // // Vérifiez si le numéro de téléphone correspond
-        // if (user.phoneNumber !== phoneNumberCot) {
-        //     console.log(user.phoneNumber);
-        //     console.log(phoneNumberCot);
-        //     return res.status(400).json({ message: 'Le numéro de téléphone ne correspond pas à l\'utilisateur',
-        //     user, phoneNumberCot
-        // });
-        // }
-
-        // Créez la cotisation en associant l'ID de l'utilisateur
-        const CotisationMtn = new Cotisation({ cotisation, user: user._id });
-        await CotisationMtn.save();
-        res.json({ message: 'Cotisation Registration success' });
-  
-    } catch (error) {
-      next(error);
+    if (!user) {
+      return res.status(404).json({ message: 'Aucun utilisateur avec ce numéro de téléphone' });
     }
-  };
 
-  module.exports = { addCotisation };
+    // Créez la cotisation en associant l'ID de l'utilisateur
+    const CotisationMtn = new Cotisation({ cotisation, user: user._id });
+    await CotisationMtn.save();
+    res.json({ message: 'Enregistrement de la cotisation réussi' });
+
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { addCotisation };
