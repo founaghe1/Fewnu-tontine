f58253008a7dac3ce57b2cbd243d1c2354837461
Mohamed Diallo <mohameddiallo.msd11@gmail.com>
2023-10-30 15:54:06 +0000

    configuration  service worker1

--- src/service-worker.js
+++ src/service-worker.js
@@ -133,3 +133,13 @@ self.addEventListener('message', (event) => {
     self.skipWaiting();
   }
 });
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(async function () {
+    try {
+      return await fetch(event.request);
+    } catch (err) {
+      return caches.match(event.request);
+    }
+  }());
+});
