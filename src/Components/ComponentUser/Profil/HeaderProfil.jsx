7295cea869292d020c43f1c9cb73c737f9a87a97
Mohamed Diallo <mohameddiallo.msd11@gmail.com>
2023-11-02 15:27:54 +0000

    Update profil

--- src/Components/ComponentUser/Profil/HeaderProfil.jsx
+++ src/Components/ComponentUser/Profil/HeaderProfil.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import image from "../../../Assets/photo-profil.png";
 
 const HeaderProfil = () => {
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+    
+
+    
+
+  useEffect(() => {
+    // Récupérez le nom de l'utilisateur depuis le localStorage
+    const storedUser = localStorage.getItem("userData");
+
+    // Assurez-vous que les données existent et sont valides
+    if (storedUser) {
+      const userData = JSON.parse(storedUser);
+      setFirstName(userData.user.firstName);
+      setLastName(userData.user.lastName);
+      
+    }
+  }, []);
+
+
   return (
     <div id="header-profil" className="">
       <div className="d-flex justify-content-center">
@@ -9,8 +30,8 @@ const HeaderProfil = () => {
           <img src={image} alt="photo profil" />
         </div>
       </div>
-      <h1 className="text-capitalize text-center">faly seck</h1>
-      <p className="profession text-capitalize text-center">designer</p>
+      <h1 className="text-capitalize text-center">{firstName} {lastName}</h1>
+      {/* <p className="profession text-capitalize text-center">designer</p> */}
     </div>
   );
 };
