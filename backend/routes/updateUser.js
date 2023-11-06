0b8a6590c0236251343328a7209b4c7572b56178
Mohamed Diallo <mohameddiallo.msd11@gmail.com>
2023-11-06 17:49:56 +0000

    UpdatePassword old and new2

--- backend/routes/updateUser.js
+++ backend/routes/updateUser.js
@@ -8,7 +8,7 @@ const { update, updatePassword } = require('../controllers/auth');
 router.put('/updateUser/:id', update);
 
 // Définissez la route de mise à jour en utilisant le verbe HTTP PUT.
-router.post('/updatePassword/:id', updatePassword);
+router.put('/updatePassword/:id', updatePassword);
 
 // Exportez le routeur.
 module.exports = router;
