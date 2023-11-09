bd6a9e32e33c77af9643e0fec4d25154e958853f
Mohamed Diallo <mohameddiallo.msd11@gmail.com>
2023-11-09 11:29:48 +0000

    Participer ou Quitter une tontine1

--- src/Components/ComponentUser/Typetontine/TypeTontine.jsx
+++ src/Components/ComponentUser/Typetontine/TypeTontine.jsx
@@ -9,6 +9,7 @@ import imgton3 from '../../../Assets/img-ton3.png'
 
 const TypeTontine = () => {
   const [tontines, setTontines] = useState([]);
+  const userId = localStorage.getItem('userId');
 
   useEffect(() => {
     axios.get('https://fewnu-tontin.onrender.com/tontines/getTontines')
@@ -16,46 +17,36 @@ const TypeTontine = () => {
         setTontines(response.data);
       })
       .catch((error) => {
-        console.error('Error fetching tontines:', error);
+        console.error('Erreur lors de la récupération des tontines :', error);
       });
   }, []);
 
-  const handleParticipate = (tontineId, userId) => {
-    axios.post(`https://fewnu-tontin.onrender.com/participateTontine/participateTontine/${tontineId}/${userId}`)
-      .then(response => {
-        console.log(response.data);
-        // Update the local state by fetching the updated tontines
-        axios.get('https://fewnu-tontin.onrender.com/tontines/getTontines')
-          .then((response) => {
-            setTontines(response.data);
-          })
-          .catch((error) => {
-            console.error('Error fetching tontines:', error);
-          });
-      })
-      .catch(error => {
-        console.error('Error participating in tontine:', error);
-      });
-  };
+  const handleParticipate = (tontineId) => {
+    // Récupérez l'userId de l'utilisateur connecté depuis le localStorage
+    const storedUser = localStorage.getItem("userData");
+    if (storedUser) {
+      const userData = JSON.parse(storedUser);
+      const userId = userData.user._id;
 
-  const handleLeave = (tontineId, userId) => {
-    axios.post(`https://fewnu-tontin.onrender.com/leaveTontine/leaveTontine/${tontineId}/${userId}`)
-      .then(response => {
-        console.log(response.data);
-        // Update the local state by fetching the updated tontines
-        axios.get('https://fewnu-tontin.onrender.com/tontines/getTontines')
-          .then((response) => {
-            setTontines(response.data);
-          })
-          .catch((error) => {
-            console.error('Error fetching tontines:', error);
-          });
-      })
-      .catch(error => {
-        console.error('Error leaving tontine:', error);
-      });
-  };
+      console.log("userId : ",userData.user._id);
 
+      axios.post(`https://fewnu-tontin.onrender.com/addTontine/participateTontine/${tontineId}/${userId}`)
+        .then(response => {
+          console.log(response.data);
+          // Mettez à jour l'état local en récupérant les tontines mises à jour
+          axios.get('https://fewnu-tontin.onrender.com/tontines/getTontines')
+            .then((response) => {
+              setTontines(response.data);
+            })
+            .catch((error) => {
+              console.error('Erreur lors de la récupération des tontines :', error);
+            });
+        })
+        .catch(error => {
+          console.error('Erreur lors de la participation à la tontine :', error);
+        });
+    }
+  };
 
   return (
     <Layout>
@@ -76,7 +67,6 @@ const TypeTontine = () => {
               des={tontine.cotisationDay}
               some={tontine.somme}
               onParticipate={() => handleParticipate(tontine._id)} 
-              onLeave={() => handleLeave(tontine._id)}
             />
           ))}
         </div>
