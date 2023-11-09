9f58f1967133a7440b79333e6e4ee65477d3c44a
Mohamed Diallo <mohameddiallo.msd11@gmail.com>
2023-11-09 10:47:08 +0000

    Participer ou Quitter une tontine

--- backend/routes/addTontine.js
+++ backend/routes/addTontine.js
@@ -1,7 +1,7 @@
 // Importe le module Express.js.
 const express = require('express');
 // Importe les contrôleurs 'addTontine', 'participateTontine' et 'leaveTontine'.
-const { addTontine, participateTontine, leaveTontine } = require('../controllers/addTontine');
+const { addTontine, participateInTontine, leaveTontine } = require('../controllers/addTontine');
 
 // Crée un nouvel routeur Express.
 const router = express.Router();
@@ -12,7 +12,7 @@ router.post('/addTontine', addTontine);
 
 // Définit une route POST avec l'URL '/participateTontine/:tontineId/:userId'.
 // Lorsqu'une requête POST est reçue à cette URL, elle sera gérée par la fonction 'participateTontine' du contrôleur.
-router.post('/participateTontine/:tontineId/:userId', participateTontine);
+router.post('/participateTontine/:tontineId/:userId', participateInTontine);
 
 // Définit une route POST avec l'URL '/leaveTontine/:tontineId/:userId'.
 // Lorsqu'une requête POST est reçue à cette URL, elle sera gérée par la fonction 'leaveTontine' du contrôleur.
