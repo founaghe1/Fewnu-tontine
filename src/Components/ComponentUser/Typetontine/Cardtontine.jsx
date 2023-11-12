b95094d3e7f931c75e7b5d3f9f0cbbcf39747bc6
Mohamed Diallo <mohameddiallo.msd11@gmail.com>
2023-11-12 15:36:50 +0000

    participer ou quitter une tontine

--- src/Components/ComponentUser/Typetontine/Cardtontine.jsx
+++ src/Components/ComponentUser/Typetontine/Cardtontine.jsx
@@ -1,51 +1,59 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./tontine.css";
 
 const Cardtontine = (props) => {
-    const [isParticipating, setIsParticipating] = useState(() => {
-      const storedStatus = localStorage.getItem(`${props.titre}-participation`);
-      console.log(`${props.titre}-participation from localStorage:`, storedStatus);
-      return storedStatus === "true" || false;
-    });
-  
-    const handleButtonClick = () => {
-      if (isParticipating && props.onLeave) {
-        props.onLeave();
-        setIsParticipating(false);
-        localStorage.setItem(`${props.titre}-participation`, "false");
-      } else if (!isParticipating && props.onParticipate) {
-        props.onParticipate();
-        setIsParticipating(true);
-        localStorage.setItem(`${props.titre}-participation`, "true");
-      }
-    };
-  
-    return (
-      <div className="d-flex justify-content-center">
-        <div className="carte rounded-4 mb-3  d-flex">
-          <div className="left-side rounded-start-4 "></div>
-          <div className="right-side py-2 px-3">
-            <div className="top d-flex justify-content-between align-items-center mt-2 mb-1">
-              <p className="titreC">{props.titre}</p>
-              <button
-                onClick={handleButtonClick}
-                className={`btn ${isParticipating ? 'btn-secondary' : 'btn-success'}`}
-                aria-label={isParticipating ? "Quitter" : "Participer"}
-              >
-                {isParticipating ? "Quitter" : "Participer"}
-              </button>
-            </div>
-            <div className="bottom d-flex justify-content-between">
-              <p className="des">
-                <span>{props.des}</span>
-              </p>
-              <p className="statut text-capitalize pe-2">{props.some}</p>
-            </div>
+  const tontineKey = `${props.titre}-participation`;
+
+  const [isParticipating, setIsParticipating] = useState(() => {
+    const storedStatus = localStorage.getItem(tontineKey);
+    return storedStatus === "true" || false;
+  });
+
+  useEffect(() => {
+    const storedStatus = localStorage.getItem(tontineKey);
+    setIsParticipating(storedStatus === "true" || false);
+  }, [tontineKey]);
+
+  const handleButtonClick = () => {
+    if (isParticipating && props.onLeave) {
+      props.onLeave();
+      setIsParticipating(false);
+      localStorage.setItem(tontineKey, "false");
+    } else if (!isParticipating && props.onParticipate) {
+      props.onParticipate();
+      setIsParticipating(true);
+      localStorage.setItem(tontineKey, "true");
+    }
+  };
+
+  return (
+    <div className="d-flex justify-content-center">
+      <div className="carte rounded-4 mb-3  d-flex">
+        <div className="left-side rounded-start-4 "></div>
+        <div className="right-side py-2 px-3">
+          <div className="top d-flex justify-content-between align-items-center mt-2 mb-1">
+            <p className="titreC">{props.titre}</p>
+            <button
+              onClick={handleButtonClick}
+              className={`btn ${
+                isParticipating ? "btn-secondary" : "btn-success"
+              }`}
+              aria-label={isParticipating ? "Quitter" : "Participer"}
+              disabled={!isParticipating} // Disable the button if not participating
+            >
+              {isParticipating ? "Quitter" : "Participer"}
+            </button>
+          </div>
+          <div className="bottom d-flex justify-content-between">
+            <p className="des">
+              <span>{props.des}</span>
+            </p>
+            <p className="statut text-capitalize pe-2">{props.some}</p>
           </div>
         </div>
       </div>
-    );
-  };
-  
-  export default Cardtontine;
-  
+    </div>
+  );
+};
+
+export default Cardtontine;
--- src/Components/ComponentUser/Typetontine/TypeTontine.jsx
+++ src/Components/ComponentUser/Typetontine/TypeTontine.jsx
@@ -9,9 +9,7 @@ import imgton3 from '../../../Assets/img-ton3.png'
 
 const TypeTontine = () => {
   const [tontines, setTontines] = useState([]);
-
-  // Utilisez le localStorage pour stocker les tontines auxquelles l'utilisateur participe
-  const participatingTontines = JSON.parse(localStorage.getItem('participatingTontines')) || {};
+  const [participatingTontines, setParticipatingTontines] = useState({});
 
   useEffect(() => {
     axios.get('https://fewnu-tontin.onrender.com/tontines/getTontines')
@@ -21,6 +19,10 @@ const TypeTontine = () => {
       .catch((error) => {
         console.error('Erreur lors de la récupération des tontines :', error);
       });
+
+    // Fetch the participating tontines from localStorage and update state
+    const storedParticipatingTontines = JSON.parse(localStorage.getItem('participatingTontines')) || {};
+    setParticipatingTontines(storedParticipatingTontines);
   }, []);
 
   const handleParticipate = (tontineId) => {
@@ -29,14 +31,14 @@ const TypeTontine = () => {
       const userData = JSON.parse(storedUser);
       const userId = userData.user._id;
 
-      // Stockez l'information de participation dans le localStorage
+      // Update the participating tontines in localStorage
       participatingTontines[tontineId] = true;
       localStorage.setItem('participatingTontines', JSON.stringify(participatingTontines));
 
       axios.post(`https://fewnu-tontin.onrender.com/addTontine/participateTontine/${tontineId}/${userId}`)
         .then(response => {
           console.log(response.data);
-          // Mettez à jour l'état local en récupérant les tontines mises à jour
+          // Fetch the updated tontines and update state
           axios.get('https://fewnu-tontin.onrender.com/tontines/getTontines')
             .then((response) => {
               setTontines(response.data);
@@ -57,13 +59,14 @@ const TypeTontine = () => {
       const userData = JSON.parse(storedUser);
       const userId = userData.user._id;
 
-      // Supprimez l'information de participation du localStorage
+      // Update the participating tontines in localStorage
       delete participatingTontines[tontineId];
       localStorage.setItem('participatingTontines', JSON.stringify(participatingTontines));
 
       axios.post(`https://fewnu-tontin.onrender.com/addTontine/leaveTontine/${tontineId}/${userId}`)
         .then(response => {
           console.log(response.data);
+          // Fetch the updated tontines and update state
           axios.get('https://fewnu-tontin.onrender.com/tontines/getTontines')
             .then((response) => {
               setTontines(response.data);
@@ -98,7 +101,7 @@ const TypeTontine = () => {
               some={tontine.somme}
               onParticipate={() => handleParticipate(tontine._id)}
               onLeave={() => handleLeave(tontine._id)}
-              isParticipating={participatingTontines[tontine._id]} // Ajoutez cette prop pour indiquer si l'utilisateur participe
+              isParticipating={participatingTontines[tontine._id]} // Add this prop to indicate if the user is participating
             />
           ))}
         </div>
