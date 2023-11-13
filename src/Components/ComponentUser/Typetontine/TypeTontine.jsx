0989c6ecb76b856076415986ffd6fcd5bdbde91b
Mohamed Diallo <mohameddiallo.msd11@gmail.com>
2023-11-13 12:49:28 +0000

    participer ou quitter une tontine6

--- src/Components/ComponentUser/Typetontine/TypeTontine.jsx
+++ src/Components/ComponentUser/Typetontine/TypeTontine.jsx
@@ -52,6 +52,7 @@ const TypeTontine = () => {
           setTontines(response.data);
         })
         .catch((error) => {
+          
           console.error('Erreur lors de la récupération des tontines :', error);
         });
     } catch (error) {
