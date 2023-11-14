bacbb8d9fb8071d3df8c86968b13fe746f4ba826
Mohamed Diallo <mohameddiallo.msd11@gmail.com>
2023-11-14 22:15:17 +0000

    Participer ou Quitter une Tontine Persisitance data5

--- backend/routes/getCheckParticipant.js
+++ backend/routes/getCheckParticipant.js
@@ -7,20 +7,30 @@ router.get('/checkParticipation/:userId/:tontineId', async (req, res) => {
     const userId = req.params.userId;
     const tontineId = req.params.tontineId;
 
-    // Vérifiez si l'utilisateur participe à la tontine (vous devrez adapter cela à votre modèle de données)
+    // Vérifiez si l'utilisateur participe à la tontine
     const isParticipating = await checkUserParticipation(userId, tontineId);
 
     res.json({ isParticipating });
   } catch (error) {
-    console.error('Erreur lors de la vérification du statut de participation:', error);
+    console.error('Erreur lors de la vérification du statut de participation :', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
-// Fonction pour vérifier la participation (exemple - adaptez-le à votre modèle de données)
+// Fonction pour vérifier la participation
 async function checkUserParticipation(userId, tontineId) {
-  const tontine = await Tontine.findById(tontineId);
-  return tontine.participants.includes(userId);
+  try {
+    // Récupérez la tontine avec les participants
+    const tontine = await Tontine.findById(tontineId).populate('participants', '_id');
+
+    // Vérifiez si l'utilisateur participe en recherchant son identifiant dans le tableau d'identifiants d'utilisateurs
+    const isParticipating = tontine.participants.some(participant => participant._id.equals(userId));
+
+    return isParticipating;
+  } catch (error) {
+    console.error('Erreur lors de la vérification de la participation de l\'utilisateur à la tontine :', error);
+    return false;
+  }
 }
 
 module.exports = router;
