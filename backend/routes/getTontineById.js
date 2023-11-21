414a395c41184bb5054451dc9c907b71649a0ae3
Mohamed Diallo <mohameddiallo.msd11@gmail.com>
2023-11-21 09:50:10 +0000

    affichage des tontine participées

--- backend/routes/getTontineById.js
+++ backend/routes/getTontineById.js
@@ -2,14 +2,11 @@ const express = require('express');
 const router = express.Router();
 const Tontine = require('../models/Tontine');
 
-
-
 // Endpoint to get tontine details by ID
 router.get('/getTontineById/:tontineId', async (req, res) => {
-
   try {
     const tontineId = req.params.tontineId;
-    const tontine = Tontine.find(tontine => tontine._id === tontineId);
+    const tontine = await Tontine.findOne({ _id: tontineId }).exec();
 
     if (tontine) {
       res.json(tontine);
@@ -20,11 +17,6 @@ router.get('/getTontineById/:tontineId', async (req, res) => {
     console.error('Erreur lors de la récupération des tontines :', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
-
-
-
-
-  
 });
 
 module.exports = router;
