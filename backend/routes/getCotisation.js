39c8c21b4f05f6e486d3b1bf2ce8e99acfc88504
Mohamed Diallo <mohameddiallo.msd11@gmail.com>
2023-11-23 14:09:48 +0000

    filtre les cotisation selon user2

--- backend/routes/getCotisation.js
+++ backend/routes/getCotisation.js
@@ -1,18 +1,15 @@
-// getCotisations.js
 const express = require('express');
 const router = express.Router();
-const Cotisation = require('../models/Cotisation');
-const authenticate = require('../middlewares/auth');
+const Cotisation = require('../models/Cotisation'); // importer du modèle de cotisation
 
-router.get('/getCotisations', authenticate, async (req, res, next) => {
+router.get('/getCotisations', async (req, res, next) => {
   try {
-    const userId = req.user._id; // Now you can safely access req.user._id
+    
+    // Récupérez toutes les cotisations depuis la base de données
+    const cotisations = await Cotisation.find(); 
 
-    // Récupérez les cotisations de l'utilisateur actuel depuis la base de données
-    const cotisations = await Cotisation.find({ user: userId });
-
-    // Renvoyez les cotisations de l'utilisateur actuel en tant que réponse JSON
-    res.json(cotisations);
+    // Renvoyez les cotisations en tant que réponse JSON
+    res.json(cotisations); 
   } catch (error) {
     next(error);
   }
