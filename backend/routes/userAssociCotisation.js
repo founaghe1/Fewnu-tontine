3f30458d28be68c0fdd895048733ca0aade06a6f
Mohamed Diallo <mohameddiallo.msd11@gmail.com>
2023-11-26 16:24:11 +0000

    Associate user id to cotisation2

--- backend/routes/userAssociCotisation.js
+++ backend/routes/userAssociCotisation.js
@@ -5,7 +5,7 @@ const User = require('../models/User');
 router.get('/userAssociCotisation', async (req, res, next) => {
   try {
     // Assuming you have implemented authentication and the user ID is available in the request
-    const userId = req.user.id; // Replace with the actual way you retrieve the user ID from the authentication context
+    const userId = req.user._id; // Replace with the actual way you retrieve the user ID from the authentication context
 
     // Retrieve the profile information of the authenticated user
     const userProfile = await User.findById(userId);
