da1a02c23cbb281fc6ecbf98bc5b8eabe97f4ebf
Mohamed Diallo <mohameddiallo.msd11@gmail.com>
2023-12-05 13:34:13 +0000

    send email nodemailler12

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -2,7 +2,7 @@
 // const mongoose = require('mongoose');
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
-// const nodemailer = require("nodemailer");
+const nodemailer = require("nodemailer");
 const config = require("../config");
 
 
